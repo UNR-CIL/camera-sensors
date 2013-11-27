e06a341679bdcd7e0c8782e507e52a411518eded
John Jusayan <john@renocollective.com>
2013-11-27 10:57:44 -0800

    Initial Commit

---
+++ SensorsTests/SensorsTests.m
@@ -0,0 +1,34 @@
+//
+//  SensorsTests.m
+//  SensorsTests
+//
+//  Created by John Jusayan on 11/27/13.
+//  Copyright (c) 2013 Treeness, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SensorsTests : XCTestCase
+
+@end
+
+@implementation SensorsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
